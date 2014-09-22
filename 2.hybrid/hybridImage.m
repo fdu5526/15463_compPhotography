dd8cfd8353e0e56caa0efd7034807f0f8c625f38
Dave Yan <fdu5526@live.com>
2014-09-22 18:50:28 -0400

    has preliminary working stuff"

--- 2.hybrid/hybridImage.m
+++ 2.hybrid/hybridImage.m
@@ -1,7 +1,11 @@
 function [img] = hybridImage(im1, im2, cutoff_low, cutoff_high)
 	g = fspecial('gaussian', [10, 10], 10)
-	img = imfilter(im1, g,'same');
 	
+	img1 = imfilter(im1, g,'same');
+	img2 = im2 - imfilter(im2, g,'same');
 
-
-
+	for i = 1:size(img1, 1)
+    for j = 1:size(img1, 2)
+      img(i,j) = img1(i,j) + img2(i,j);
+    end
+  end
