c8491e3d3ed01142f17dcd97da8ef1a4b6820b33
Dave Yan <fdu5526@gmail.com>
2014-10-11 23:23:18 -0400

    has horizontal seams

--- 3.seam/seamCarve.py
+++ 3.seam/seamCarve.py
@@ -41,46 +41,6 @@ def generateGradients(width, height, pixels):
 
 
 
-# find a horizontal seam, return indices
-def findHorizontalSeam(gradientList, width, height):
-	size = width*height
-
-	# calculate gradient perculation
-	for x in range(width, size):
-		g1 = gradientList[x - width]
-		g2 = posInf if(x % width == 0) else gradientList[x - width - 1]
-		g3 = posInf if((x + 1) % width == 0) else gradientList[x - width + 1]
-		gradientList[x] = gradientList[x] + min(g1, g2, g3)
-
-	# generate the seam starting piont
-	minSeamStartIndex = 0
-	minSeamStart = posInf
-	for y in range(size - width, size):
-		if(gradientList[y] < minSeamStart):
-			minSeamStart = gradientList[y]
-			minSeamStartIndex = y
-	seam = [minSeamStartIndex]
-
-	# generate rest of the seam
-	seamIndex = minSeamStartIndex
-	while(seamIndex >= width):
-		g1 = gradientList[seamIndex - width]
-		g2 = posInf if(seamIndex % width == 0) else gradientList[seamIndex - width - 1]
-		g3 = posInf if((seamIndex+1) % width == 0) else gradientList[seamIndex - width + 1]
-		ming = min(g1,g2,g3)
-		bestIndex = seamIndex - width
-		if(g2 == ming):
-			bestIndex = seamIndex - width - 1
-		elif(g3 == ming):
-			bestIndex = seamIndex - width + 1
-		seam.append(bestIndex)
-		seamIndex = bestIndex
-
-	return seam
-
-
-
-
 # find a vertical seam, return indices
 def findVerticalSeam(gradientList, width, height):
 	size = width*height
@@ -119,14 +79,29 @@ def findVerticalSeam(gradientList, width, height):
 	return seam
 
 
+
+
+
 fileName = sys.argv[1]
 amountToCarve = int(sys.argv[2])
+
+removeHorizontalSeams = True
 
 try:
 	# load the image
 	im = Image.open(fileName)
 	pixels = list(im.getdata())
 	w,h = im.size
+
+	# transpose for horizontal removal
+	if(removeHorizontalSeams):
+		newPixels = []
+		for x in (reversed (range(0,w))):
+			for y in ((range(0,h))):
+				newPixels.append(pixels[y*w + x])
+		pixels = newPixels
+		w,h = h,w
+
 
 	for i in range(0,amountToCarve):
 		print (str(100*i/amountToCarve) + "%")
@@ -136,14 +111,26 @@ try:
 		# find the seam
 		seam = findVerticalSeam(gradientList, w, h)
 
+		#print(str(len(seam)) + " " + str(h))
+
 		# remove the seam
 		for s in seam:
 			pixels.pop(s)
 		w = w - 1
 		
 
+	if(removeHorizontalSeams):
+		# transpose for horizontal removal
+		if(removeHorizontalSeams):
+			newPixels = []
+			for x in ( (range(0,w))):
+				for y in (reversed (range(0,h))):
+					newPixels.append(pixels[y*w + x])
+			pixels = newPixels
+			w,h = h,w
+
 	# save the image
-	im2 = Image.new(im.mode, (len(pixels)/h, h))
+	im2 = Image.new(im.mode, (w, h))
 	im2.putdata(pixels)
 	im2.save(fileName+"-test.jpg")
 		
