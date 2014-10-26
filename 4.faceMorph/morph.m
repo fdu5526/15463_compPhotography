47574d0cc2dde55d0c426b7b7740bc2a4873ff0f
Dave Yan <fdu5526@live.com>
2014-10-25 22:27:31 -0400

    got affine matrix

--- 4.faceMorph/morph.m
+++ 4.faceMorph/morph.m
@@ -1,9 +1,42 @@
 function [output] = morph(im1, im2, im1_pts, im2_pts, tri, warp_frac, dissolve_frac)
-	computeAffine([],[]);
-	output = 0.5 * im1 + 0.5 * im2;
+	
+	im1_2 = im1;
+	im2_2 = im2;
+
+	for i = 1:(size(tri,1))
+		im1_tri = [im1_pts(tri(i,1), 1), 
+							 im1_pts(tri(i,1), 2),
+							 im1_pts(tri(i,2), 1), 
+							 im1_pts(tri(i,2), 2),
+							 im1_pts(tri(i,3), 1), 
+							 im1_pts(tri(i,3), 2)];
+		im2_tri = [im2_pts(tri(i,1), 1), 
+							 im2_pts(tri(i,1), 2),
+							 im2_pts(tri(i,2), 1), 
+							 im2_pts(tri(i,2), 2),
+							 im2_pts(tri(i,3), 1), 
+							 im2_pts(tri(i,3), 2)];
+
+		average_tri = (1 - dissolve_frac) * im1_tri + dissolve_frac * im2_tri;
+
+		affine1 = computeAffine(im1_tri, average_tri);
+		affine2 = computeAffine(im2_tri, average_tri);
+		
+		
+		% mytsearch, interp2
+
+	end
+
+
+	output = (1 - dissolve_frac) * im1_2 + dissolve_frac * im2_2;
 
 
 
 function [output] = computeAffine(tri1_pts,tri2_pts)
-	% mytsearch, interp2
-	output = [];
+	A = [[tri1_pts(1),tri1_pts(2), 1, 0, 0, 0],
+			 [0, 0, 0, tri1_pts(1),tri1_pts(2), 1],
+			 [tri1_pts(3),tri1_pts(4), 1, 0, 0, 0],
+			 [0, 0, 0, tri1_pts(3),tri1_pts(4), 1],
+			 [tri1_pts(5),tri1_pts(6), 1, 0, 0, 0],
+			 [0, 0, 0, tri1_pts(5),tri1_pts(6), 1]];
+	output = inv(A) * tri2_pts;
--- 4.faceMorph/morphFace.m
+++ 4.faceMorph/morphFace.m
@@ -1,19 +1,21 @@
 user_name1 = 'syan';
 user_name2 = 'tesker';
 
-im1 = imread([ 'pictures\' user_name1 '.JPG' ]);
-im2 = imread([ 'pictures\' user_name2 '.JPG' ]);
+im1 = im2double(imread([ 'pictures\' user_name1 '.JPG' ]));
+im2 = im2double(imread([ 'pictures\' user_name2 '.JPG' ]));
 im1_pts = importdata([ 'data\' user_name1 '.txt' ]);
 im2_pts = importdata([ 'data\' user_name2 '.txt' ]);
 
 %%%%%%%%%%%%%%%%%% end initial loading %%%%%%%%%%%%%%%%%%
 
-% delauney points, get middle, compute
-
+% delauney points for triangles
 average_pts = 0.5*(im1_pts + im2_pts);
 triangles = delaunay(average_pts(:, 1), average_pts(:, 2));
 
-morphed_im = morph(im1, im2, im1_pts, im2_pts, triangles, 0.5, 0.5);
+frameCount = 0;
+for i = 0:frameCount
+	morphed_im = morph(im1, im2, im1_pts, im2_pts, triangles, i/frameCount, i/frameCount);
+end
 
 
 %imshow(morphed_im)
