1775ed47a86c1c949b81f6a8d274278628f6b565
Dave Yan <fdu5526@live.com>
2014-11-18 01:02:14 -0500

    start warping

--- 6.stitch/computeH.m
+++ 6.stitch/computeH.m
@@ -1,2 +1,20 @@
 function [H] = computeH(im1_pts,im2_pts)
-	H = [];
+	%https://stackoverflow.com/questions/16345656/homography-matrix-for-image-stitching-in-matlab
+
+	n = size(im1_pts, 1); 
+  A = zeros(n*2,8); 
+  b = zeros(n*2,1); 
+  j=1;
+  for i=1:n
+      A(j,:)=[im2_pts(i,1), im2_pts(i,2), 1, 0, 0, 0, -im1_pts(i,1)*im2_pts(i,1), -im1_pts(i,1)*im2_pts(i,2)];
+      b(j,1)=im1_pts(i,1);
+      j=j+1;
+      A(j,:)=[0, 0, 0, im2_pts(i,1), im2_pts(i,2), 1, -im1_pts(i,2)*im2_pts(i,1), -im1_pts(i,2)*im2_pts(i,2)];
+      b(j,1)=im1_pts(i,2);
+      j=j+1;
+  end
+
+  x = (A\b);
+  H = [x(1,1), x(2,1), x(3,1);
+       x(4,1), x(5,1), x(6,1);
+       x(7,1), x(8,1), 1];
--- 6.stitch/warpImage.m
+++ 6.stitch/warpImage.m
@@ -1,2 +1,26 @@
 function [imwarped] = warpImage(im,H)
-	imwarped = [];
+	
+	sizeIm = size(im);
+	imNew = zeros(sizeIm(1), sizeIm(2), sizeIm(3));
+
+	for y = 1:sizeIm(1)
+		for x = 1:sizeIm(2)
+			p = [x,
+					 y,
+					 1];
+			newP = p;
+			newP = newP / newP(3);
+
+			nx = round(newP(1));
+			ny = round(newP(2));
+
+			% TODO make this real
+			%if (sizeIm(2) >= nx & nx >= 1 & sizeIm(1) >= nx & nx >= 1)
+				imNew(ny, nx, :) = im(y ,x ,:);
+			%end
+
+		end
+	end
+
+
+	imwarped = 1/256 * imNew;
